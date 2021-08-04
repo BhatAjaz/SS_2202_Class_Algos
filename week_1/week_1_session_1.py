b8b61ee1ed788d0df980b4ddd10dd5583f290394
ajaz706 <ajaz.bhat@ubd.edu.bn>
2021-08-04 10:03:50 +0800

    Create week_1_session_1.py

---
+++ week_1/week_1_session_1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 07:43:45 2021
+
+@author: ajaz7
+"""
+from math import *
+
+y = log(10)
+j = 5
+i = j//2
+
+name = 'Haziq'
+name[1]
+
+first_name = 'Ajaz'
+last_name = 'Bhat'
+
+full_name = first_name +' ' + last_name
+
+#nameken = len(full_name)
+
+#print (len(full_name))
+
+nested = [[2,[37],4, 'hello']]
+my_list = nested[:]
+nested[0][0] =12
+
+marks = 9
+
+if marks > 10:
+    grade = 'A'
+elif marks > 5:
+    grade = 'B'
+else:
+    grade = 'C'
+test_var = 5
+    
+for i in range(5):
+    print(i)
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
